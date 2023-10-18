3102ccb58f73921b68a6a53e8c4abf341c166602
Enigma Turing <kexauso613@gmail.com>
2023-10-18 14:55:45 +0700

    gen

---
+++ coding/usaco/bronze/basic_complete_search/bovine_genomics.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int n, m; cin >> n >> m;
+    vector<string> s(n), p(n);
+
+    for(int i = 0; i < n; i++)
+        cin >> s[i];
+    for(int i = 0; i < n; i++)
+        cin >> p[i];
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
