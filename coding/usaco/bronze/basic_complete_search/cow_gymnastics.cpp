d0556cab850510aa3b5b67d7bacfa7e6f3839903
Truong Giang <kexauso613@gmail.com>
2023-10-18 11:23:33 +0700

    gym

---
+++ coding/usaco/bronze/basic_complete_search/cow_gymnastics.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int k, n; cin >> k >> n;
+    vector<vector<int>> a(k, vector<int>(n));
+    for(int i = 0; i < k; i++)
+        for(int j = 0; j < n; j++)
+            cin >> a[i][j];
+    
+    int result = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            bool check = true;
+            for(int t = 0; t < k; t++)
+                if(a[t][i] >= a[t][j])
+                {
+                    check = false;
+                    break;
+                }
+            if(check) result++;
+        }
+    }
+    cout << result;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
