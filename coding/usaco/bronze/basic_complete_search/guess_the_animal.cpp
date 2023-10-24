8fd6210783c2e0038dc2da3a647c9817aa853194
ghibli <kexauso613@gmail.com>
2023-10-24 11:08:10 +0700

    guess

--- coding/usaco/bronze/basic_complete_search/guess_the_animal.cpp
+++ coding/usaco/bronze/basic_complete_search/guess_the_animal.cpp
@@ -6,18 +6,31 @@ typedef long long ll;
 void solve()
 {
     int n; cin >> n;
-    vector<vector<string>> a;
+    vector<set<string>> a;
     for(int i = 0; i < n; i++)
     {
-    	int m; cin >> m;
-    	vector<string> tmp;
-    	for(int j = 0; j < m; j++) 
-    	{
-    		string s; cin >> s;
-    		tmp.push_back(s);
-    	}
-    	a.push_back(tmp);
-    } 
+        string name; cin >> name;
+        int charac_num; cin >> charac_num;
+        set<string> cows;
+        for(int j = 0; j < charac_num; j++)
+        {
+            string charac; cin >> charac;
+            cows.insert(charac);
+        }
+        a.push_back(cows);
+    }
+
+    int ans = 0;
+
+    for(int i = 0; i < n-1; i++)
+        for(int j = i + 1; j < n; j++)
+        {
+            int tmp_ans = 0;
+            for(set<string>::iterator charac = a[i].begin(); charac != a[i].end(); charac++)
+                if(a[j].find(*charac) != a[j].end()) tmp_ans++;
+            if(tmp_ans > ans) ans = tmp_ans;
+        }
+    cout << ans+1;
 }
 
 int main()
