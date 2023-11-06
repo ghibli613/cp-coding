0a6b6b7c84bc40d9bf0cdbc81307492ed7c5fd63
Truong Giang <kexauso613@gmail.com>
2023-11-06 10:13:45 +0700

    done_shake

--- coding/usaco/bronze/basic_complete_search/cowntact_tracing.cpp
+++ coding/usaco/bronze/basic_complete_search/cowntact_tracing.cpp
@@ -16,13 +16,13 @@ void solve()
     for(int i = 0; i < T; i++)
     {
         int t, x, y; cin >> t >> x >> y;
-        get<0>(a[i]) = t;
-        get<1>(a[i]) = x;
-        get<2>(a[i]) = y;
+        get<0>(a[i]) = t - 1;
+        get<1>(a[i]) = x - 1;
+        get<2>(a[i]) = y - 1;
     }
     sort(a.begin(), a.end(), compar_tuple);
 
-    auto is_consistent = [&](int patient_zero, int k) 
+    function<bool(int, int)> is_consistent = [&](int patient_zero, int k) 
     {
         vector<bool> tmp_infected(N);
         vector<int> num_shakes(N);
@@ -30,16 +30,33 @@ void solve()
         for (tuple<int, int, int> sh : a) 
         {
             if (tmp_infected[get<0>(sh)]) { num_shakes[get<0>(sh)]++; }
-            if (tmp_infected[sh.y]) { num_shakes[sh.y]++; }
-            if (num_shakes[sh.x] <= k && tmp_infected[sh.x]) {
-                tmp_infected[sh.y] = true;
+            if (tmp_infected[get<1>(sh)]) { num_shakes[get<1>(sh)]++; }
+            if (num_shakes[get<0>(sh)] <= k && tmp_infected[get<0>(sh)]) {
+                tmp_infected[get<1>(sh)] = true;
             }
-            if (num_shakes[sh.y] <= k && tmp_infected[sh.y]) {
-                tmp_infected[sh.x] = true;
+            if (num_shakes[get<1>(sh)] <= k && tmp_infected[get<1>(sh)]) {
+                tmp_infected[get<0>(sh)] = true;
             }
         }
+        for (int i = 0; i < N; i++) 
+			if (tmp_infected[i] && s[i] == '1') 
+                return false; 
+        return true;
     };
 
+    int min_k = T, max_k = -1;
+    set<int> possible_patient;
+    for(int patient_zero = 0; patient_zero < N; patient_zero++)
+        for(int k = 0; k <= T; k++)
+            if(is_consistent(patient_zero, k))
+            {
+                possible_patient.insert(patient_zero);
+                min_k = min(min_k, k);
+                max_k = max(max_k, k);
+            }
+    if(max_k == T)
+        cout << possible_patient.size() << ' ' << min_k << " Infinity";
+    else cout << possible_patient.size() << ' ' << min_k << ' ' << max_k;
 }
 
 int main()
