3383c5610b5eb6b2d770bc0ee5502cf019a8e56b
Truong Giang <kexauso613@gmail.com>
2023-11-14 14:56:07 +0700

    done_moo

--- coding/usaco/bronze/basic_complete_search/moo_language.cpp
+++ coding/usaco/bronze/basic_complete_search/moo_language.cpp
@@ -28,8 +28,86 @@ void solve()
     	if (noun_cnt < 0) break;
 
     	int type_2 = min({(int)tverb.size(), noun_cnt / 2, min(conj_cnt, period) * 2 + max(0, period - conj_cnt)});
+    	cur_words += 3 * type_2;
+    	noun_cnt -= 2 * type_2;
 
+    	int total = type_1 + type_2;
+    	int connections = type_1 + type_2 - 1;
+    	int can_combine = min((connections + 1) / 2, conj_cnt);
+    	cur_words += can_combine;
+
+    	period -= total - can_combine;
+    	if(period < 0) continue;
+
+    	int tack = 0;
+    	if (type_2 > 0)
+    	{
+    	 	tack = min(noun_cnt, comma);
+    	 	cur_words += tack;
+    	} 
+    	if(cur_words > ans)
+    	{
+    		ans = cur_words;
+    		combine = can_combine;
+    		t1 = type_1;
+    		t2 = type_2;
+    		tack_end = tack;
+    	}
     }
+
+    cout << ans << '\n';
+
+    if(ans == 0)
+    {
+    	cout << '\n';
+    	return;
+    }
+
+    vector<vector<string>> sentences;
+    for(int i = 0; i < t1; i++)
+    {
+    	sentences.push_back({noun.back(), iverb.back()});
+    	noun.pop_back();
+    	iverb.pop_back();
+    }
+    for(int i = 0; i < t2; i++)
+    {
+    	sentences.push_back({noun.back(), tverb.back()});
+    	noun.pop_back();
+    	tverb.pop_back();
+    	sentences.back().push_back(noun.back());
+    	noun.pop_back();
+    }
+   	
+   	string output;
+   	for(int i = 0; i < (int)sentences.size(); i++)
+   	{
+   		for(string j : sentences[i]) output += j + " ";
+   		if(i % 1 == 0 && combine)
+   		{
+   			combine--;
+   			output += conj.back() + " ";
+   			conj.pop_back();
+   		}
+   		else
+   		{
+   			output.pop_back();
+   			output += ". ";
+   		}
+   	}
+
+   	output.pop_back();
+	if(tack_end > 0)
+	{
+		output.pop_back();
+		for(int i = 0; i < tack_end; i++)
+		{
+			output += ", " + noun.back();
+			noun.pop_back();
+		}
+		output += ".";
+	}
+	cout << output << '\n';
 }
 
 int main()
