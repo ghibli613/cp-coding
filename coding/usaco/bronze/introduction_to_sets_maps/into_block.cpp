0237a1b17a775de8d28262232a643a40e7879333
Truong Giang <kexauso613@gmail.com>
2023-12-01 10:58:07 +0700

    done_block

--- coding/usaco/bronze/introduction_to_sets_maps/into_block.cpp
+++ coding/usaco/bronze/introduction_to_sets_maps/into_block.cpp
@@ -5,7 +5,31 @@ typedef long long ll;
 
 void solve()
 {
+    int n, q; cin >> n >> q;
+    vector<int> a(n);
+    map<int, int> num, last;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        num[a[i]]++;
+        last[a[i]] = i;
+    }
     
+    // segment
+    int l = 0, r = -1, ans = 0, max_num = 0;
+    for(int i = 0; i < n; i++)
+    {
+        r = max(r, last[a[i]]);
+        max_num = max(max_num, num[a[i]]);
+        if(i == r)
+        {
+            ans += r - l + 1 - max_num;
+            l = r + 1;
+            max_num = 0;
+            r = 0;
+        }
+    }
+    cout << ans;
 }
 
 int main()
@@ -14,7 +38,7 @@ int main()
     cin.tie(NULL);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
 
     while(t--)
         solve();
