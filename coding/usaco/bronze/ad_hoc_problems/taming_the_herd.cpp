af079427361e0af2de83642bb0901bd7440ede3c
ghibli <kexauso613@gmail.com>
2023-12-03 01:31:41 +0700

    done_break

--- coding/usaco/bronze/ad_hoc_problems/taming_the_herd.cpp
+++ coding/usaco/bronze/ad_hoc_problems/taming_the_herd.cpp
@@ -5,7 +5,50 @@ typedef long long ll;
 
 void solve()
 {
-    
+    int n; cin >> n;
+    vector<int> logs(n);
+    int max_stay = -1, cnt_stay = 0, pos = -1, min_break = 1, tmp = 0;
+    bool check = true;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> logs[i];
+        if(logs[i] != -1)
+        {
+            max_stay++;
+            if(logs[i] > max_stay) 
+            {
+                cout << -1;
+                return;
+            }
+            else if(logs[i] == max_stay)
+            {
+                if(check)
+                {
+                    check = false;
+                    tmp = max_stay;
+                    cnt_stay += tmp;;
+                }
+                cnt_stay -= tmp;
+                tmp = max_stay = logs[i];
+                cnt_stay += tmp;
+                pos = i;
+            }
+            else
+            {
+                if(i - pos <= logs[i])
+                {
+                    cout << -1;
+                    return;
+                }
+                tmp = max_stay = logs[i];
+                cnt_stay += tmp;
+                pos = i;
+                min_break++;
+            }
+        }
+        else max_stay++;
+    }
+    cout << min_break << " " << n - cnt_stay;
 }
 
 int main()
@@ -14,7 +57,7 @@ int main()
     cin.tie(NULL);
 
     int t = 1;
-    cin >> t;
+    // cin >> t;
 
     while(t--)
         solve();
