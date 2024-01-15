b7f34ed79b168062a91a5cfd1ae4cae93e5de2ce
ghibli613 <kexauso613@gmail.com>
2024-01-15 19:16:22 +0700

    done

--- coding/codeforces/ct/C.cpp
+++ coding/codeforces/ct/C.cpp
@@ -1,51 +1,64 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-
-template<typename T, typename V>
-void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
-template<typename T>
-void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
-void _print() {cerr << "]\n";}
-template <typename T, typename... V>
-void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-#define dbg(...) cerr<<__func__<<":"<<__LINE__<<" [" << #__VA_ARGS__ << "] = ["; _print(__VA_ARGS__);
-
 typedef long long ll;
 
 void solve()
 {
-    int n; cin >> n;
-    vector<int> l(n), r(n);
-    vector<ll> c(n);
-    for(int &i : l) cin >> i;
-    for(int &i : r) cin >> i;
-    for(ll &i : c) cin >> i;
-    sort(l.begin(), l.end());
-    sort(r.begin(), r.end());
-    sort(c.begin(), c.end());
-    vector<int> intervals(n);
-    for(int i = 0; i < n; i++)
-        intervals[i] = r[i] - l[i];
-    
-    sort(intervals.begin(), intervals.end(), greater<int>());
-    vector<int> s_intervals();
-    ll ans = 0;
-    dbg(intervals);
-    cout << ans << '\n';
+    int n; ll k; cin >> n >> k;
+    vector<ll> a(n);
+    for(int i = 0; i < n; i++) 
+        cin >> a[i];
+    if(k > 2) cout << 0 << '\n';
+    else 
+    {
+        if(k == 1)
+        {
+            ll ans = LONG_LONG_MAX;
+            sort(a.begin(), a.end());
+            for(int i = 0; i < n - 1; i++)
+                if(ans > a[i + 1] - a[i])
+                    ans = a[i + 1] - a[i];
+            for(int i = 0; i < n; i++)
+                if(ans > a[i])
+                    ans = a[i];
+            cout << ans << '\n';
+        }
+        else
+        {
+            set<ll> ms; ll ans = LONG_LONG_MAX;
+            for(int i = 0; i < n; i++)
+                if(ans > a[i])
+                    ans = a[i];
+            for(int i = 0; i < n - 1; i++)
+                for(int j = i + 1; j < n; j++)
+                {
+                    ll tmp = abs(a[i] - a[j]);
+                    if(ans > tmp)
+                        ans = tmp;
+                    if(tmp == 0)
+                    {
+                        cout << 0 << '\n';
+                        return;
+                    }
+                    else
+                        ms.insert(tmp);
+                }
+            
+            for(ll i = 0; i < n; i++)
+            {
+                std::set<ll>::iterator itlow = ms.lower_bound(a[i]);
+                if(itlow !=  ms.end() && ans > *itlow - a[i])
+                    ans = *itlow - a[i];
+                
+                if(itlow != ms.begin() && ans > a[i] - *(--itlow))
+                    ans = a[i] - *itlow;
+
+            }
+            cout << ans << '\n';
+
+        }
+    }
 }
 
 int main()
