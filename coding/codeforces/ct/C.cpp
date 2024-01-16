f017feae461235cb7b4eba23d72bfa566e120354
ghibli613 <kexauso613@gmail.com>
2024-01-17 00:21:45 +0700

    done

--- coding/codeforces/ct/C.cpp
+++ coding/codeforces/ct/C.cpp
@@ -5,60 +5,19 @@ typedef long long ll;
 
 void solve()
 {
-    int n; ll k; cin >> n >> k;
-    vector<ll> a(n);
-    for(int i = 0; i < n; i++) 
-        cin >> a[i];
-    if(k > 2) cout << 0 << '\n';
-    else 
+    int n; cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    vector<pair<int, int>> p(n);
+    for(int i = 0; i < n; i++)
     {
-        if(k == 1)
-        {
-            ll ans = LONG_LONG_MAX;
-            sort(a.begin(), a.end());
-            for(int i = 0; i < n - 1; i++)
-                if(ans > a[i + 1] - a[i])
-                    ans = a[i + 1] - a[i];
-            for(int i = 0; i < n; i++)
-                if(ans > a[i])
-                    ans = a[i];
-            cout << ans << '\n';
-        }
-        else
-        {
-            set<ll> ms; ll ans = LONG_LONG_MAX;
-            for(int i = 0; i < n; i++)
-                if(ans > a[i])
-                    ans = a[i];
-            for(int i = 0; i < n - 1; i++)
-                for(int j = i + 1; j < n; j++)
-                {
-                    ll tmp = abs(a[i] - a[j]);
-                    if(ans > tmp)
-                        ans = tmp;
-                    if(tmp == 0)
-                    {
-                        cout << 0 << '\n';
-                        return;
-                    }
-                    else
-                        ms.insert(tmp);
-                }
-            
-            for(ll i = 0; i < n; i++)
-            {
-                std::set<ll>::iterator itlow = ms.lower_bound(a[i]);
-                if(itlow !=  ms.end() && ans > *itlow - a[i])
-                    ans = *itlow - a[i];
-                
-                if(itlow != ms.begin() && ans > a[i] - *(--itlow))
-                    ans = a[i] - *itlow;
-
-            }
-            cout << ans << '\n';
-
-        }
+        p[i].first = a[i];
+        p[i].second = i;
     }
+    sort(p.begin(), p.end(), greater<int>());
+    vector<int> l(n);
+    l[0] = 0; int max_e = p[0].first;
+    
 }
 
 int main()
