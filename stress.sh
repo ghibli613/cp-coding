a43bb06900a7c7d5bad272ad08ea49e634b605e7
Bùi Trường Giang <85751029+ghibli613@users.noreply.github.com>
2024-01-23 19:28:31 +0700

    Update stress.sh

--- stress.sh
+++ stress.sh
@@ -1,13 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
+ 
+ read -p 'The number of test cases: ' num_tests
 
-for i in {1..100}; do
-    echo $i
-
-    $1 $i 4 5 > _in.txt
-    $2 < _in.txt > _out.txt
+for ((testNum=0;testNum<$num_tests;testNum++))
+do
+    $1 > _in.txt
     $3 < _in.txt > _ans.txt
-
-    diff _out.txt _ans.txt || exit 1
+    $2 < _in.txt > _out.txt
+    H1=`md5sum _out.txt`
+    H2=`md5sum _ans.txt`
+    if !(cmp -s "_out.txt" "_ans.txt")
+    then
+        echo "Error found!"
+        exit
+    fi
 done
-
-echo "Finished"
+echo Passed $num_tests test cases!
