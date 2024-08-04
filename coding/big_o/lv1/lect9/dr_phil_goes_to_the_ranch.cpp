9f3d0748cd2847239ab2f270c6488ddf8643e831
ghibli613 <kexauso613@gmail.com>
2024-08-04 19:03:59 +0700

    done lect9

---
+++ coding/big_o/lv1/lect9/dr_phil_goes_to_the_ranch.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void solve()
+{
+    int n, x = 0, pre = 0; cin >> n;
+    stack<int> missed;
+    vector<bool> available(n + 1);
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x;
+        if(pre < x)
+        {
+            cout << x << ' ';
+            while(++pre < x)
+            {
+                missed.push(pre);
+            }
+        }
+        available[x] = true;
+        while(!missed.empty() && available[missed.top()])
+        {
+            cout << missed.top() << ' ';
+            missed.pop();
+        }
+    }
+    while (!missed.empty())
+    {
+        cout << missed.top() << " ";
+        missed.pop();
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
