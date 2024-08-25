811cc3cdbdc2030fc755b14a8dd741e021d02d55
giang.bui <giang.bui@opswat.com>
2024-08-25 09:41:49 +0700

    add morer template

---
+++ coding/big_o/lv1/lect15/rare_order.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+void solve()
+{
+    
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--)
+        solve();
+
+    return 0;
+}
