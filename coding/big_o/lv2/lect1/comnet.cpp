0cd5f25626b4324e82a9691886441b24a9193f3b
ghibli613 <kexauso613@gmail.com>
2024-08-27 23:30:33 +0700

    done

--- coding/big_o/lv2/lect1/comnet.cpp
+++ coding/big_o/lv2/lect1/comnet.cpp
@@ -4,35 +4,63 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-struct Edge
+struct DisjointSet
 {
-    int u, v, w;
-    Edge(int u = 0, int v = 0, int w = 0)
-        : u(u), v(v), w(w) {}
+    vector<int> parent, Rank;
+    DisjointSet(int n): parent(n + 1), Rank(n + 1, 0)
+    {
+        for (int i=1; i<=n; i++) parent[i] = i;
+    }
+    int findSet(int u)
+    {
+        if (parent[u] != u) parent[u] = findSet(parent[u]);
+        return parent[u];
+    }
+    bool unionSet(int u, int v)
+    {
+        int pu = findSet(u), pv = findSet(v);
+        if(pu == pv) return false;
+        if(Rank[pu] > Rank[pv]) parent[pv] = pu;
+        else if(Rank[pu] < Rank[pv]) parent[pu] = pv;
+        else
+        {
+            parent[pv] = pu;
+            Rank[pu]++;
+        }
+        return true;
+    }
 };
-
-vector<Edge> e;
-vector<vector<pii>> adj;
 
 void solve()
 {
     int n, m, q; cin >> n >> m >> q;
-    e = vector<Edge>(m);
-    adj.assign(n, vector<pii>());
+    vector<pii> e(m);
+    vector<int> w(m);
     for(int i = 0; i < m; i++)
     {
-        Edge tmp; cin >> tmp.u >> tmp.v >> tmp.w;
-        tmp.u--; tmp.v--;
-        e[i] = tmp;
-        adj[tmp.u].push_back({tmp.v, tmp.w});
-        adj[tmp.v].push_back({tmp.u, tmp.w});
+        cin >> e[i].first >> e[i].second >> w[i];
     }
     
     while(q--)
     {
         int k, s; cin >> k >> s;
-        
+        k--;
+        vector<int> ww = w;
+        while(s--)
+        {
+            int t, c; cin >> t >> c;
+            ww[t - 1] = c;
+        }
+        DisjointSet a(n);
+        for(int i = 0; i < m; i++) 
+            if(ww[i] < ww[k]) 
+            {
+                a.unionSet(e[i].first, e[i].second);
+            }
+        if (a.unionSet(e[k].first, e[k].second)) cout << "NO\n";
+        else cout << "YES\n";
     }
+    
 }
 
 int main()
