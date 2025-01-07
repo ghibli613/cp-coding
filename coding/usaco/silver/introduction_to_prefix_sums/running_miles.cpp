72bb2f4af524a3d6b894542c94a2ff03e8705f7c
giang.bui <giang.bui@opswat.com>
2025-01-07 17:14:48 +0700

    do some usaco

--- coding/usaco/silver/introduction_to_prefix_sums/running_miles.cpp
+++ coding/usaco/silver/introduction_to_prefix_sums/running_miles.cpp
@@ -1,23 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
-void solve()
-{
-    
-}
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    
+	int test_num;
+	cin >> test_num;
 
-    int t = 1;
-    cin >> t;
+	for (int t = 0; t < test_num; t++) {
+		int n;
+		cin >> n;
+		vector<int> a(n);
+		for (int i = 0; i < n; i++) { cin >> a[i]; }
 
-    while(t--)
-        solve();
+		vector<int> pref_max(n);
+		vector<int> suff_max(n);
+		for (int i = 0; i < n; i++) {
+			pref_max[i] = a[i] + i;
+			suff_max[i] = a[i] - i;
+		}
 
-    return 0;
+		for (int i = 1; i < n; i++) { pref_max[i] = max(pref_max[i], pref_max[i - 1]); }
+		for (int i = n - 2; i >= 0; i--) {
+			suff_max[i] = max(suff_max[i], suff_max[i + 1]);
+		}
+
+		int ans = 0;
+		for (int i = 1; i < n - 1; i++) {
+			ans = max(ans, pref_max[i - 1] + a[i] + suff_max[i + 1]);
+		}
+
+		cout << ans << '\n';
+	}
 }
