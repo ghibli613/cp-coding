0e9e3589988bf141604843ef0e3215b6ad8c4f37
ghibli613 <kexauso613@gmail.com>
2025-04-17 23:39:25 +0700

    done

---
+++ coding/big_o/lv5/lect6/square.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int M, sum, sticks[20];
+int memo[1 << 20];
+
+int search(int length, int bitMask)
+{
+    if (memo[bitMask] != -1)
+        return memo[bitMask];
+
+    if (length > sum / 4)
+        return 0;
+    else if (length == sum / 4)
+    {
+        if (bitMask == (1 << M) - 1)
+            return 1;
+        length = 0;
+    }
+
+    for (int i = 0; i < M; ++i)
+        if ((bitMask & 1 << i) == 0 && search(length + sticks[i], bitMask | 1 << i))
+        {
+            return 1;
+        }
+    return memo[bitMask] = 0;
+}
+
+int solve()
+{
+    sum = accumulate(sticks, sticks + M, 0);
+    if (sum % 4 != 0)
+        return 0;
+
+    memset(memo, -1, sizeof memo);
+    return search(0, 0);
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+    while (N--)
+    {
+        cin >> M;
+        for (int i = 0; i < M; ++i)
+            cin >> sticks[i];
+
+        if(solve())
+            cout << "yes\n";
+        else
+            cout << "no\n";
+    }
+    return 0;
+}
